b95b991a67015aef73dbb1b3c53568c83d3317e1
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-08 20:42:01 +0000

    Various test changes

--- test/pond.fish
+++ test/pond.fish
@@ -1,16 +1,20 @@
 # basic commands
 
-@test 'can create pond' (pond create test >/dev/null 2>&1) $status -eq 0
+@test 'create pond succeeds' (pond create test >/dev/null 2>&1) $status -eq 0
 
-@test 'can list ponds' (pond list >/dev/null 2>&1) $status -eq 0
+@test 'list ponds succeeds' (pond list >/dev/null 2>&1) $status -eq 0
 
-@test 'can remove pond' (echo 'y' | pond remove test >/dev/null 2>&1) $status -eq 0
+@test 'remove pond succeeds' (echo 'y' | pond remove test >/dev/null 2>&1) $status -eq 0
+
+@test 'list ponds fails when no ponds exist' (pond list >/dev/null 2>&1) $status -eq 1
 
 # stdout tests
 
-@test 'create pond reports success' (pond create test) = "pond: Created an empty pond named 'test'"
+@test 'create pond reports correctly' (pond create test) = "pond: Created an empty pond named 'test'"
 
-@test 'list pond reports success' (pond list) = "pond: Found the following ponds:
+@test 'list pond reports correctly' (pond list | string collect) = "pond: Found the following ponds:
   test"
 
-@test 'remove pond reports success' (echo 'y' | pond remove test) = "pond: Removed pond 'test'"
+@test 'remove pond reports correctly' (echo 'y' | pond remove test) = "pond: Removed pond 'test'"
+
+@test 'list pond reports correctly when no ponds exist' (pond list) = "pond: No ponds found; create one with 'create'"

7ba48a5df3731570bc926eaa895a648feef6d71a
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-08 21:50:08 +0000

    Add missing command name in tests

--- test/vars.fish
+++ test/vars.fish
@@ -1,7 +1,7 @@
 set fail 1
 set success 0
 
-# variable commands
+# variable command tests
 
 pond create test
 
@@ -9,8 +9,14 @@ pond create test
 
 @test 'set pond variable fails for a non-existent pond' (pond var set none ABC 123 >/dev/null 2>&1) $status -eq $fail
 
-@test 'get pond variable succeeds' (pond get test ABC >/dev/null 2>&1) $status -eq $success
+@test 'get pond variable succeeds' (pond var get test ABC >/dev/null 2>&1) $status -eq $success
 
-@test 'get pond variable fails for a non-existent variable' (pond get test NONE >/dev/null 2>&1) $status -eq $fail
+@test 'get pond variable fails for a non-existent variable' (pond var get test NONE >/dev/null 2>&1) $status -eq $fail
 
-@test 'get pond variable fails for a non-existent pond' (pond get none ABC >/dev/null 2>&1) $status -eq $fail
+@test 'get pond variable fails for a non-existent pond' (pond var get none ABC >/dev/null 2>&1) $status -eq $fail
+
+@test 'remove pond variable succeeds' (pond var remove test ABC >/dev/null 2>&1) $status -eq $success
+
+@test 'remove pond variable fails for a non-existent variable' (pond var remove test NONE >/dev/null 2>&1) $status -eq $fail
+
+@test 'remove pond variable fails for a non-existent pond' (pond var remove none ABC >/dev/null 2>&1) $status -eq $fail

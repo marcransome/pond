41d795cd2bb2c7d083fb580f5701c343aefc85ff
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-08 20:35:34 +0000

    Fix tests

--- test/pond.fish
+++ test/pond.fish
@@ -2,9 +2,9 @@
 
 @test 'can create pond' (pond create test >/dev/null 2>&1) $status -eq 0
 
-@test 'can remove pond' (echo 'y' | pond remove test >/dev/null 2>&1) $status -eq 0
-
 @test 'can list ponds' (pond list >/dev/null 2>&1) $status -eq 0
+
+@test 'can remove pond' (echo 'y' | pond remove test >/dev/null 2>&1) $status -eq 0
 
 # stdout tests
 

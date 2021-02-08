58557d6190bd3f14208fdea534a40af1edd709b0
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-08 17:09:27 +0000

    Source function files

--- conf.d/pond.fish
+++ conf.d/pond.fish
@@ -1,7 +1,8 @@
 function _pond_install --on-event pond_install
-    set -U pond_tree $__fish_config_dir/ponds
-    set -U pond_vars env_vars.fish
-    set -U pond_prefix pond
+    set -U pond_tree" $__fish_config_dir/ponds"
+    set -U pond_vars "env_vars.fish"
+    set -U pond_funcs "functions"
+    set -U pond_prefix "pond"
 end
 
 function _pond_uninstall --on-event pond_uninstall
@@ -19,8 +20,12 @@ function _pond_uninstall --on-event pond_uninstall
 end
 
 function _pond_init
-    for file in $pond_tree/*/$pond_vars
-        source $file
+    for vars in $pond_tree/*/$pond_vars
+        source $vars
+    end
+
+    for func in $pond_tree/*/$pond_funcs/*.fish
+        source $func
     end
 end
 

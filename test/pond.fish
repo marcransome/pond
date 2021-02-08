0be9423bea8451f5a7eef7e5cb0cc05394954273
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-08 20:29:22 +0000

    Add additional tests

--- test/pond.fish
+++ test/pond.fish
@@ -1,3 +1,16 @@
-@test 'can create pond' (pond create test > /dev/null 2>&1 ) $status -eq 0
+# basic commands
+
+@test 'can create pond' (pond create test >/dev/null 2>&1) $status -eq 0
 
 @test 'can remove pond' (echo 'y' | pond remove test >/dev/null 2>&1) $status -eq 0
+
+@test 'can list ponds' (pond list >/dev/null 2>&1) $status -eq 0
+
+# stdout tests
+
+@test 'create pond reports success' (pond create test) = "pond: Created an empty pond named 'test'"
+
+@test 'list pond reports success' (pond list) = "pond: Found the following ponds:
+  test"
+
+@test 'remove pond reports success' (echo 'y' | pond remove test) = "pond: Removed pond 'test'"

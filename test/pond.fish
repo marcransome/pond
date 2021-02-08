7cee53f5cf767c6885e3bb6e983f01155d0a80e1
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-08 22:26:09 +0000

    Add variables to pond tests

--- test/pond.fish
+++ test/pond.fish
@@ -1,8 +1,9 @@
 set fail 1
 set success 0
+set test_pond pond
 set pond_version 0.2.0
 
-# pond commands
+# pond command tests
 
 @test 'short version option succeeds' (pond -v >/dev/null 2>&1) $status -eq $success
 
@@ -24,21 +25,21 @@ set pond_version 0.2.0
 
 @test 'list ponds fails when no ponds exist' (pond list >/dev/null 2>&1) $status -eq $fail
 
-# output tests
+# pond command output tests
 
 @test 'short version option reports correctly' (pond -v) = "pond $pond_version"
 
 @test 'long version option reports correctly' (pond --version) = "pond $pond_version"
 
-@test 'create pond reports correctly' (pond create test) = "pond: Created an empty pond named 'test'"
+@test 'create pond reports correctly' (pond create $test_pond) = "pond: Created an empty pond named '$test_pond'"
 
-@test 'create pond reports correctly when pond already exists' (pond create test 2>&1) = "pond: A pond named 'test' already exists"
+@test 'create pond reports correctly when pond already exists' (pond create $test_pond 2>&1) = "pond: A pond named '$test_pond' already exists"
 
 @test 'list pond reports correctly when ponds exist' (pond list | string collect) = "pond: Found the following ponds:
   test"
 
-@test 'remove pond reports correctly when pond exists' (echo 'y' | pond remove test) = "pond: Removed pond 'test'"
+@test 'remove pond reports correctly when pond exists' (echo 'y' | pond remove $test_pond) = "pond: Removed pond '$test_pond'"
 
-@test 'remove pond reports correctly when pond does not exist' (pond remove test 2>&1) = "pond: A pond named 'test' does not exist"
+@test 'remove pond reports correctly when pond does not exist' (pond remove $test_pond 2>&1) = "pond: A pond named '$test_pond' does not exist"
 
 @test 'list pond reports correctly when no ponds exist' (pond list 2>&1) = "pond: No ponds found; create one with 'create'"

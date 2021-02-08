459ed6e4b51317bab314f0572c4756ccbbb6816d
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-08 21:41:32 +0000

    Create test pond as prerequisite for tests

--- test/vars.fish
+++ test/vars.fish
@@ -3,6 +3,8 @@ set success 0
 
 # variable commands
 
+pond create test
+
 @test 'set pond variable succeeds' (pond var set test ABC 123 >/dev/null 2>&1) $status -eq $success
 
 @test 'set pond variable fails for a non-existent pond' (pond var set none ABC 123 >/dev/null 2>&1) $status -eq $fail

047ec6b98e814ee70021e60a11f6a2ec337f06f7
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-02-14 21:07:29 +0000

    Refactor load command tests

--- test/load.fish
+++ test/load.fish
@@ -2,8 +2,7 @@ set fail 1
 set success 0
 
 set -x __pond_under_test yes
-set pond_name pond
-set success_output "Pond loaded: $pond_name"
+set pond_name test-pond
 
 set command_usage "\
 Usage:
@@ -12,63 +11,70 @@ Usage:
 Arguments:
     name  The name of the pond to load"
 
-function __pond_setup
-    pond create -e $pond_name
+function __pond_setup_regular
+    pond create -e $pond_name >/dev/null 2>&1
+    for var in TEST_VAR_{1,2,3}
+        set -xg $var (string lower $var)
+        echo "set -xg $var "(string lower $var) >>  $pond_home/$pond_regular/$pond_name/$pond_vars
+    end
 end
 
-function __pond_add_global_exports
-    echo "\
-set -xg GLOBAL_EXPORT_ONE 1
-set -xg GLOBAL_EXPORT_TWO 2
-set -xg GLOBAL_EXPORT_THREE 3
-" >> $pond_data/$pond_name/$pond_vars
+function __pond_setup_private
+    pond create -e -p $pond_name >/dev/null 2>&1
+    for var in TEST_VAR_PRIVATE_{1,2,3}
+        set -xg $var (string lower $var)
+        echo "set -xg $var "(string lower $var) >>  $pond_home/$pond_private/$pond_name/$pond_vars
+    end
 end
 
 function __pond_tear_down
-    echo 'y' | pond remove $pond_name
+    for var in TEST_VAR{,_PRIVATE}_{1,2,3}
+        set -e $var
+    end
+    echo "y" | pond remove $pond_name >/dev/null 2>&1
 end
 
-@echo 'pond load: success tests'
-__pond_setup
-@test 'pond load: success for empty pond' (pond load $pond_name >/dev/null 2>&1) $status -eq $success
-@test 'pond load: output success for empty pond' (pond load $pond_name 2>&1) = $success_output
-__pond_add_global_exports
-@test 'pond load: success for pond with global exports' (pond load $pond_name >/dev/null 2>&1) $status -eq $success
-@test 'pond load: output success for pond with global exports' (pond load $pond_name 2>&1) = $success_output
-@test 'pond load: global variable one was set' (echo $GLOBAL_EXPORT_ONE) = "1"
-@test 'pond load: global variable two was set' (echo $GLOBAL_EXPORT_TWO) = "2"
-@test 'pond load: global variable three was set' (echo $GLOBAL_EXPORT_THREE) = "3"
+@echo "pond load: success tests for regular pond"
+__pond_setup_regular
+@test "setup: pond variables exist" -n (read < $pond_home/$pond_regular/$pond_name/$pond_vars)
+@test "pond load: success exit code" (pond load $pond_name >/dev/null 2>&1) $status -eq $success
+@test "pond load: test variable one was set" (echo $TEST_VAR_1) = "test_var_1"
+@test "pond load: test variable two was set" (echo $TEST_VAR_2) = "test_var_2"
+@test "pond load: test variable three was set" (echo $TEST_VAR_3) = "test_var_3"
 __pond_tear_down
 
-@echo 'pond load: failure exit code tests'
-__pond_setup
-@test 'pond load: fails for missing pond name' (pond load >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for trailing arguments' (pond load $pond_name trailing >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for malformed pond name' (pond load _invalid >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for short invalid option' (pond load -i >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for long invalid option' (pond load --invalid >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for valid short option and missing pond name' (pond load -e >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for valid long option and missing pond name' (pond load --empty >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for invalid short option and valid pond name' (pond load -i $pond_name >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for invalid long option and valid pond name' (pond load --invalid $pond_name >/dev/null 2>&1) $status -eq $fail
-@test 'pond load: fails for non-existent pond' (pond load non-exist >/dev/null 2>&1) $status -eq $fail
+@echo "pond load: output tests for regular pond"
+__pond_setup_regular
+@test "pond load: success output message" (pond load $pond_name 2>&1) = "Loaded pond: $pond_name"
 __pond_tear_down
 
-@echo 'pond load: failure usage output tests'
-__pond_setup
-@test 'pond load: command usage shown for missing pond name' (pond load 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for trailing arguments' (pond load $pond_name trailing 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for malformed pond name' (pond load _invalid 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for short invalid option' (pond load -i 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for long invalid option' (pond load --invalid 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for valid short option and missing pond name' (pond load -e 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for valid long option and missing pond name' (pond load --empty 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for invalid short option and valid pond name' (pond load -i $pond_name 2>&1 | string collect) = $command_usage
-@test 'pond load: command usage shown for invalid long option and valid pond name' (pond load --invalid $pond_name 2>&1 | string collect) = $command_usage
-@test 'pond load: command error shown for non-existent pond' (pond load non-exist 2>&1 | string collect) = "Pond does not exist: non-exist"
+@echo "pond load: success tests for private pond"
+__pond_setup_private
+@test "setup: pond variables exist" -n (read < $pond_home/$pond_private/$pond_name/$pond_vars)
+@test "pond load: success exit code" (pond load $pond_name >/dev/null 2>&1) $status -eq $success
+@test "pond load: test variable one was set" (echo $TEST_VAR_PRIVATE_1) = "test_var_private_1"
+@test "pond load: test variable two was set" (echo $TEST_VAR_PRIVATE_2) = "test_var_private_2"
+@test "pond load: test variable three was set" (echo $TEST_VAR_PRIVATE_3) = "test_var_private_3"
 __pond_tear_down
 
-set -e __pond_setup
-set -e __pond_add_global_exports
+@echo "pond load: output tests for private pond"
+__pond_setup_private
+@test "pond load: success output message" (pond load $pond_name 2>&1) = "Loaded private pond: $pond_name"
+__pond_tear_down
+
+@echo "pond load: validation failure exit code tests"
+@test "pond load: fails for missing pond name" (pond load >/dev/null 2>&1) $status -eq $fail
+@test "pond load: fails for trailing arguments" (pond load $pond_name trailing >/dev/null 2>&1) $status -eq $fail
+@test "pond load: fails for malformed pond name" (pond load _invalid >/dev/null 2>&1) $status -eq $fail
+@test "pond load: fails for non-existent pond" (pond load no-exist >/dev/null 2>&1) $status -eq $fail
+
+@echo "pond load: validation failure output tests"
+@test "pond load: command usage shown for missing pond name" (pond load 2>&1 | string collect) = $command_usage
+@test "pond load: command usage shown for trailing arguments" (pond load $pond_name trailing 2>&1 | string collect) = $command_usage
+@test "pond load: command usage shown for malformed pond name" (pond load _invalid 2>&1 | string collect) = $command_usage
+@test "pond load: command usage shown for non-existent pond" (pond load no-exist 2>&1 | string collect) = "Pond does not exist: no-exist"
+
+set -e __pond_setup_regular
+set -e __pond_setup_private
 set -e __pond_tear_down
 set -e __pond_under_test

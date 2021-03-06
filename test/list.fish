570a50c37959ca999351031643afff3eda2a109b
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-03-06 22:00:05 +0000

    Add success tests for enabled option

--- test/list.fish
+++ test/list.fish
@@ -200,6 +200,32 @@ for command in "pond list" "pond list "{-p,--private}" "{-r,--regular}
 
 end
 
+for command in "pond list" "pond list "{-e,--enabled}
+
+    @echo "$command: success tests for enabled regular pond"
+    __pond_setup_single_regular
+    @test "pond list: enabled regular pond" (eval $command >/dev/null 2>&1) $status -eq $success
+    @test "pond list: output message correct" (eval $command 2>&1 | string collect) = $success_output_single_regular
+    __pond_tear_down_single_regular
+
+    @echo "$command: success tests for enabled private pond"
+    __pond_setup_single_private
+    @test "pond list: success enabled private pond" (eval $command >/dev/null 2>&1) $status -eq $success
+    @test "pond list: output message correct" (eval $command 2>&1 | string collect) = $success_output_single_private
+    __pond_tear_down_single_private
+
+    @echo "$command: success tests for multiple enabled regular ponds"
+    __pond_setup_multiple_regular
+    @test "pond list: success multiple enabled regular ponds" (eval $command >/dev/null 2>&1) $status -eq $success
+    @test "pond list: output message correct" (eval $command 2>&1 | string collect) = $success_output_multiple_regular
+    __pond_tear_down_multiple_regular
+
+    @echo "$command: success tests for multiple enabled private ponds"
+    __pond_setup_multiple_private
+    @test "pond list: success multiple enabled private ponds" (eval $command >/dev/null 2>&1) $status -eq $success
+    @test "pond list: output message correct" (eval $command 2>&1 | string collect) = $success_output_multiple_private
+    __pond_tear_down_multiple_private
+end
 for command in "pond list "{-r,--regular}
 
     @echo "$command: failure tests for missing regular ponds"

74f1807b0d9beec40e6cdb2bc7112e05c6f4070b
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-03-07 22:16:27 +0000

    Add test helper variables

---
+++ test/helpers/variables.fish
@@ -0,0 +1,13 @@
+set -x __pond_under_test yes
+set pond_version (git describe --tags --abbrev=0)
+
+set fail 1
+set success 0
+
+set pond_test_var_count 3
+
+set pond_name_regular_prefix test-pond
+set pond_name_private_prefix test-pond-private
+
+set pond_name_regular $pond_name_regular_prefix-1
+set pond_name_private $pond_name_private_prefix-1

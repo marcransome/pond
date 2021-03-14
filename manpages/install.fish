af367215e76a6b0c0c4cfd7fa6f88ab2930e8712
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-03-14 18:46:18 +0000

    Remove faulty exit code check

--- manpages/install.fish
+++ manpages/install.fish
@@ -7,10 +7,7 @@ end
 
 set http_code (curl --write-out "%{http_code}" --silent "https://raw.githubusercontent.com/marcransome/pond/main/manpages/pond.1" -o /usr/local/share/man/man1/pond.1)
 
-if test $status -ne 0
-    echo "A curl error occurred when retrieving the man page: $status"
-    exit 1
-else if test $http_code -ne 200
+if test $http_code -ne 200
     echo "A HTTP error occurred when retrieving the man page: $http_code"
     exit 1
 else

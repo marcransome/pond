f86559e8fd7fc36ef18b8e935029a330fa516ff3
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-06-05 13:22:17 +0100

    Test fix

--- tests/command.fish
+++ tests/command.fish
@@ -1,10 +1,9 @@
 source ./fixtures/variables.fish
 
-set unknown_command "unknown"
-set unknown_command_error (set_color red; and echo -n "Error: "; and set_color normal; and echo "Unknown command: $unknown_command")
+set unknown_command_error (set_color red; and echo -n "Error: "; and set_color normal; and echo "Unknown command: unknown")
 
 @echo 'pond: unknown command failure exit code tests'
-@test 'pond fails for unknown command' (pond $unknown_command >/dev/null 2>&1) $status -eq $failure
+@test 'pond fails for unknown command' (pond unknown >/dev/null 2>&1) $status -eq $failure
 
 @echo 'pond: unknown command output tests'
-@test 'pond reports correct error' (pond $unknown_command 2>&1) = $unknown_command_error
+@test 'pond reports correct error' (pond unknown 2>&1) = $unknown_command_error

46c6876eb25e4a555788831acd5327d7ed94f7db
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2021-06-05 13:26:38 +0100

    Add version check

--- tests/command.fish
+++ tests/command.fish
@@ -2,6 +2,8 @@ source ./fixtures/variables.fish
 
 set -gx TERM xterm-256color
 
+fish --version
+
 set unknown_command_error (set_color red; and echo -n "Error: "; and set_color normal; and echo "Unknown command: unknown")
 
 @echo 'pond: unknown command failure exit code tests'

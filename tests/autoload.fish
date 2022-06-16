292ea45c1ce3ec766aecafa70f4f2d2f9909b899
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2022-06-16 21:45:08 +0100

    Add autoload command validation tests

--- tests/autoload.fish
+++ tests/autoload.fish
@@ -7,9 +7,9 @@ Usage:
     pond autoload <name>
 
 Arguments:
-name  The name of the pond for which an autoload function
-      will be opened in an editor and optionally created if
-      it does not already exist" >&2
+    name  The name of the pond for which an autoload function
+          will be opened in an editor and optionally created if
+          it does not already exist" >&2
 
 @echo "pond autoload $pond_name: success tests for pond without autoload function"
 __pond_setup 1 enabled loaded unpopulated
@@ -40,3 +40,11 @@ __pond_editor_intercept_with __pond_test_autoload_editor
 __pond_tear_down
 
 __pond_editor_reset
+
+@echo "pond autoload: validation failure exit code tests"
+@test "pond autoload: fails for missing pond name" (pond autoload >/dev/null 2>&1) $status -eq $failure
+@test "pond autoload: fails for malformed pond name" (pond autoload _invalid >/dev/null 2>&1) $status -eq $failure
+
+@echo "pond autoload: validation failure output tests"
+@test "pond autoload: command usage shown for missing pond name" (pond autoload 2>&1 | string collect) = $command_usage
+@test "pond autoload: command usage shown for malformed pond name" (pond autoload _invalid 2>&1 | string collect) = $command_usage
